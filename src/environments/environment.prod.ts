5fa383ea3a7d0b35f3c0d4986c10359658657e0d
Alex Novikov <alex@asnovikov.ru>
2018-12-01 19:51:53 -0300

    updated prod environment;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  liquidLongContractAddress: '0x80F8DAA435A9AB4B1802BA56FE7E0ABD0F8AB3D3',
+  ethPricePollingFrequency: 3000,      // frequency in milliseconds
+  providerFeePollingFrequency: 3000,   // frequency in milliseconds
 };
